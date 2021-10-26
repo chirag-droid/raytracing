b8a71c29b92afe4138b527cc606a3ec27fe871e4
Chirag Singla <chirag.singla.pi@gmail.com>
2021-10-26 16:40:21 +0530

    Create the final scene

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,52 @@
 
 Color ray_color(const Ray& r, const Hittable& , int max_depth);
 
+HittableList random_scene() {
+    HittableList world;
+
+    // random elements
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            Point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - Point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<Material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = Color::random() * Color::random();
+                    sphere_material = make_shared<Lambertian>(albedo);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = Color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<Metal>(albedo, fuzz);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<Dielectric>(1.5);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    // Basic elements
+    auto ground_material = make_shared<Lambertian>(Color(0.5, 0.5, 0.5));
+    auto metal = make_shared<Metal>(Color(0.7, 0.6, 0.5), 0.0);
+    auto dielectric = make_shared<Dielectric>(1.5);
+    auto lamber = make_shared<Lambertian>(Color(0.4, 0.2, 0.1));
+
+    world.add(make_shared<Sphere>(Point3(0, -1000, 0), 1000, ground_material));
+    world.add(make_shared<Sphere>(Point3(4, 1, 0), 1.0, metal));
+    world.add(make_shared<Sphere>(Point3(0, 1, 0), 1.0, dielectric));
+    world.add(make_shared<Sphere>(Point3(-4, 1, 0), 1.0, lamber));
+
+    return world;
+}
+
 int main() {
     // Image dimensions
     auto aspect_ratio = 16.0 / 9.0;
@@ -42,18 +88,7 @@ int main() {
     Camera camera(lookfrom, lookat, vup, 20, aspect_ratio);
 
     // Create a hittable_list world
-    HittableList world;
-    auto ground_material = make_shared<Lambertian>(Color(0.5, 0.5, 0.5));
-    world.add(make_shared<Sphere>(Point3(0,-1000,0), 1000, ground_material));
-
-    auto metal = make_shared<Metal>(Color(0.7, 0.6, 0.5), 0.3);
-    world.add(make_shared<Sphere>(Point3(4, 1, 0), 1.0, metal));
-
-    auto dielectric = make_shared<Dielectric>(1.5);
-    world.add(make_shared<Sphere>(Point3(0, 1, 0), 1.0, dielectric));
-
-    auto lambertian = make_shared<Lambertian>(Color(0.4, 0.2, 0.1));
-    world.add(make_shared<Sphere>(Point3(-4, 1, 0), 1.0, lambertian));
+    auto world = random_scene();
 
     // Iterate over height and width
     for (int j = IMAGE_HEIGHT-1; j >= 0; j--) {
